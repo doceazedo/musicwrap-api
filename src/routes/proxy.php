c5084dd5d7cbd6844818e5ff6e16b6c27713b150
PxLucasF <hi@lucasfernandes.com.br>
2021-02-04 22:22:13 -0300

    ✨ Adicionar imagem de capa padrão

--- src/routes/proxy.php
+++ src/routes/proxy.php
@@ -4,5 +4,11 @@ validate_request(['url']);
 
 $url = base64_decode(REQUEST[0]);
 $img = getimagesize($url);
+
+if (empty($img)) {
+  $url = BASE_URL . '/public/img/cover.png';
+  $img = getimagesize($url);
+}
+
 header("Content-type: {$img['mime']}");
 readfile($url);
