1774ef009ac34cd4eef30d3d7cd407835e46ed16
DoceAzedo <hi@lucasfernandes.com.br>
2021-08-04 11:02:56 -0300

    📦 Implementar handlebars

--- routes/v1/generate.js
+++ routes/v1/generate.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer-core');
-
+const handlebars = require('handlebars');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const lastfm = require('../../utils/lastfm');
@@ -9,7 +9,6 @@ module.exports = async function routes (fastify, options) {
     const layout = fs.readFileSync('./themes/_layout.html').toString();
     let theme = fs.readFileSync('./themes/classic.html').toString();
 
-    // api
     const user_info = await lastfm({
       method: 'user.getinfo',
       user: request.query.user,
@@ -22,12 +21,21 @@ module.exports = async function routes (fastify, options) {
       limit: 4
     });
 
-    theme = theme
-      .replace(/{{user.playcount}}/g, top_tracks.data.toptracks['@attr'].total)
-      .replace(/{{user.avatar}}/g, user_info.data.user.image[3]['#text']);
+    const template = handlebars.compile(theme);
+    const data = {
+      user_info: user_info.data.user,
+      top_tracks: top_tracks.data.toptracks
+    };
+
+    // patch some shit
+    data.user_info.image = user_info.data.user.image[user_info.data.user.image.length - 1]['#text'];
+    data.user_info.registered = user_info.data.user.registered.unixtime;
+    data.top_tracks.attr = top_tracks.data.toptracks['@attr'];
+
+    // TODO: foreach nas tracks pra pegar imagem do deezer
 
     const $ = cheerio.load(layout);
-    $('body').html(theme);
+    $('body').html(template(data));
 
     const browser = await puppeteer.launch({
       executablePath: './bin/chrome-win/chrome.exe',
@@ -35,8 +43,8 @@ module.exports = async function routes (fastify, options) {
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1920 });
-    
     await page.setContent($.html());
+    // await page.waitForNavigation();
     await page.screenshot({ path: 'example.png' });
   
     await browser.close();
